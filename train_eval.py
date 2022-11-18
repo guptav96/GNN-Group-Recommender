6ae91a14c3f9a0bbb3cd7e8426020a2688cadec4
nnakshat <nishanthsn@live.com>
2022-11-17 19:11:29 -0500

    visualize update

--- train_eval.py
+++ train_eval.py
@@ -246,6 +246,9 @@ def eval_rmse_ensemble(model, checkpoints, loader, device, show_progress=False):
 
 
 def visualize(model, graphs, res_dir, data_name, class_values, num=5, sort_by='prediction'):
+    sort_by = "true"
+    num = 10
+
     model.eval()
     model.to(device)
     R = []
@@ -271,52 +274,57 @@ def visualize(model, graphs, res_dir, data_name, class_values, num=5, sort_by='p
     lowest_ys = [Y[i] for i in order[:num]]
     scores = highest_scores + lowest_scores
     ys = highest_ys + lowest_ys
-    type_to_label = {0: 'u0', 1: 'v0', 2: 'u1', 3: 'v1', 4: 'u2', 5: 'v2'}
-    type_to_color = {0: 'xkcd:red', 1: 'xkcd:blue', 2: 'xkcd:orange', 
-                     3: 'xkcd:lightblue', 4: 'y', 5: 'g'}
-    plt.axis('off')
-    f = plt.figure(figsize=(20, 10))
-    # axs = f.subplots(2, num)
-    # cmap = plt.cm.get_cmap('rainbow')
-    vmin, vmax = min(class_values), max(class_values)
-    # sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    # sm.set_array([])
-    for i, g in enumerate(highest + lowest):
-        pos = nx.drawing.layout.multipartite_layout(g, subset_key="layer")
-        node_colors = [type_to_color[y] for x, y in nx.get_node_attributes(g, 'type').items()]
-        nx.draw(g, pos, node_color = node_colors, with_labels = False)
-        # bottom_u_node = min(pos, key=lambda x: (pos[x][0], pos[x][1]))
-        # bottom_v_node = min(pos, key=lambda x: (-pos[x][0], pos[x][1]))
-        # # swap u0 and v0 with bottom nodes if they are not already
-        # if u0 != bottom_u_node:
-        #     pos[u0], pos[bottom_u_node] = pos[bottom_u_node], pos[u0]
-        # if v0 != bottom_v_node:
-        #     pos[v0], pos[bottom_v_node] = pos[bottom_v_node], pos[v0]
-        # labels = {x: type_to_label[y] for x, y in nx.get_node_attributes(g, 'type').items()}
-        # node_colors = [type_to_color[y] for x, y in nx.get_node_attributes(g, 'type').items()]
-        # edge_types = nx.get_edge_attributes(g, 'type')
-        # edge_types = [class_values[edge_types[x]] for x in g.edges()]
-        # axs[i//num, i%num].axis('off')
-        # nx.draw_networkx(g, pos, 
-        #         #labels=labels, 
-        #         with_labels=False, 
-        #         node_size=150, 
-        #         node_color=node_colors, edge_color=edge_types, 
-        #         ax=axs[i//num, i%num], edge_cmap=cmap, edge_vmin=vmin, edge_vmax=vmax, 
-        #         )
-        # # make u0 v0 on top of other nodes
-        # nx.draw_networkx_nodes(g, {u0: pos[u0]}, nodelist=[u0], node_size=150,
-        #         node_color='xkcd:red', ax=axs[i//num, i%num])
-        # nx.draw_networkx_nodes(g, {v0: pos[v0]}, nodelist=[v0], node_size=150,
-        #         node_color='xkcd:blue', ax=axs[i//num, i%num])
-        # axs[i//num, i%num].set_title('{:.4f} ({:})'.format(
-        #     scores[i], ys[i]), x=0.5, y=-0.05, fontsize=20
-        # )
-        break
-    # f.subplots_adjust(right=0.85)
-    # cbar_ax = f.add_axes([0.88, 0.15, 0.02, 0.7])
-    # if len(class_values) > 20:
-    #     class_values = np.linspace(min(class_values), max(class_values), 20, dtype=int).tolist()
-    # cbar = plt.colorbar(sm, cax=cbar_ax, ticks=class_values)
-    # cbar.ax.tick_params(labelsize=22)
-    plt.savefig(os.path.join(res_dir, "visualization_{}_{}.pdf".format(data_name, sort_by)), bbox_inches='tight')
+
+    print("Predicted Scores:", scores)
+    print("True Scores:", ys)
+
+
+    # type_to_label = {0: 'u0', 1: 'v0', 2: 'u1', 3: 'v1', 4: 'u2', 5: 'v2'}
+    # type_to_color = {0: 'xkcd:red', 1: 'xkcd:blue', 2: 'xkcd:orange', 
+    #                  3: 'xkcd:lightblue', 4: 'y', 5: 'g'}
+    # plt.axis('off')
+    # f = plt.figure(figsize=(20, 10))
+    # # axs = f.subplots(2, num)
+    # # cmap = plt.cm.get_cmap('rainbow')
+    # vmin, vmax = min(class_values), max(class_values)
+    # # sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+    # # sm.set_array([])
+    # for i, g in enumerate(highest + lowest):
+    #     pos = nx.drawing.layout.multipartite_layout(g, subset_key="layer")
+    #     node_colors = [type_to_color[y] for x, y in nx.get_node_attributes(g, 'type').items()]
+    #     nx.draw(g, pos, node_color = node_colors, with_labels = False)
+    #     # bottom_u_node = min(pos, key=lambda x: (pos[x][0], pos[x][1]))
+    #     # bottom_v_node = min(pos, key=lambda x: (-pos[x][0], pos[x][1]))
+    #     # # swap u0 and v0 with bottom nodes if they are not already
+    #     # if u0 != bottom_u_node:
+    #     #     pos[u0], pos[bottom_u_node] = pos[bottom_u_node], pos[u0]
+    #     # if v0 != bottom_v_node:
+    #     #     pos[v0], pos[bottom_v_node] = pos[bottom_v_node], pos[v0]
+    #     # labels = {x: type_to_label[y] for x, y in nx.get_node_attributes(g, 'type').items()}
+    #     # node_colors = [type_to_color[y] for x, y in nx.get_node_attributes(g, 'type').items()]
+    #     # edge_types = nx.get_edge_attributes(g, 'type')
+    #     # edge_types = [class_values[edge_types[x]] for x in g.edges()]
+    #     # axs[i//num, i%num].axis('off')
+    #     # nx.draw_networkx(g, pos, 
+    #     #         #labels=labels, 
+    #     #         with_labels=False, 
+    #     #         node_size=150, 
+    #     #         node_color=node_colors, edge_color=edge_types, 
+    #     #         ax=axs[i//num, i%num], edge_cmap=cmap, edge_vmin=vmin, edge_vmax=vmax, 
+    #     #         )
+    #     # # make u0 v0 on top of other nodes
+    #     # nx.draw_networkx_nodes(g, {u0: pos[u0]}, nodelist=[u0], node_size=150,
+    #     #         node_color='xkcd:red', ax=axs[i//num, i%num])
+    #     # nx.draw_networkx_nodes(g, {v0: pos[v0]}, nodelist=[v0], node_size=150,
+    #     #         node_color='xkcd:blue', ax=axs[i//num, i%num])
+    #     # axs[i//num, i%num].set_title('{:.4f} ({:})'.format(
+    #     #     scores[i], ys[i]), x=0.5, y=-0.05, fontsize=20
+    #     # )
+    #     break
+    # # f.subplots_adjust(right=0.85)
+    # # cbar_ax = f.add_axes([0.88, 0.15, 0.02, 0.7])
+    # # if len(class_values) > 20:
+    # #     class_values = np.linspace(min(class_values), max(class_values), 20, dtype=int).tolist()
+    # # cbar = plt.colorbar(sm, cax=cbar_ax, ticks=class_values)
+    # # cbar.ax.tick_params(labelsize=22)
+    # plt.savefig(os.path.join(res_dir, "visualization_{}_{}.pdf".format(data_name, sort_by)), bbox_inches='tight')
